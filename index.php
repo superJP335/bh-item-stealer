2c83d8d2f431c4daf448ba0d94ae561f75a9db31
superJP335 <76703807+superJP335@users.noreply.github.com>
2021-11-12 17:43:41 -0500

    version 1.2
    Version 1.2 Change Log:
    - Added the Advanced Sorting from the Brick-Hill shop
    - Clicking the item's image now takes you to the item page on BH
    - Link now embeds if posted on sites like Discord
    - Minor bug fixes

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
+<meta charset="UTF-8">
+<meta name="description" content="Brick Hill Item Stealer is an open-source project for getting and downloading Brick-Hill hats, shirts, pants, faces, tools, and t-shirts for free! Just type the name of the desired item, click Download Texture or Download Model and boom! It works!">
+<meta name="keywords" content="Brick building, brick build together part piece construct make create set.">
+<meta name="author" content="superJP335">
+<meta name="theme-color" content="#00A9FE">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Brick Hill Item Stealer</title>
 <link href="https://css.brkcdn.com/a0aebf55acbcb4d737f7.css" rel="stylesheet" type="text/css">
 <link href="https://css.brkcdn.com/4358064e6837331e6fcb.css" rel="stylesheet" type="text/css">
-<script src="https://js.brkcdn.com/34c1aba64069a399d9cc.js"></script>
-<script src="https://js.brkcdn.com/41575af6ef3518e77ef1.js"></script>
-<script src="https://js.brkcdn.com/4f9a079d4f96e0d70137.js"></script>
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -11,14 +14,15 @@ error_reporting(E_ALL);
 if(isset($_POST['iid'])){
 $name = $_POST['iid'];
 $type = $_POST['type'];
-$api_url = "https://api.brick-hill.com/v1/shop/list?sort=oldest&type=$type&search=$name&cursor=&limit=20&page=1";
+$sort = $_POST['sort'];
+$api_url = "https://api.brick-hill.com/v1/shop/list?sort=$sort&type=$type&search=$name&cursor=&limit=20&page=1";
 $json_data = file_get_contents($api_url);
 $response_data = json_decode($json_data);
 $item_data = $response_data->data;
 echo"<center><h1>Showing results for <q>".htmlspecialchars($name)."</q></h1>
 <h3>Lookup Item</h3>
 <form method='post'>
-<input style='width:170px;height:35px;' type='text' name='iid' placeholder='Item Name'>
+<input style='width:170px;height:35px;' autocorrect='off' type='text' name='iid' placeholder='Item Name'>
 <select style='width:170px;height:35px;' name='type' class='select'>
 <option value='all'>All</option>
 <option value='hat'>Hats</option>
@@ -29,11 +33,18 @@ echo"<center><h1>Showing results for <q>".htmlspecialchars($name)."</q></h1>
 <option value='pants'>Pants</option>
 <option value='head'>Heads</option>
 </select>
+<select style='width:170px;height:35px;' name='sort' class='select'>
+<option value='updated'>Recently Updated</option>
+<option value='newest'>Newest First</option>
+<option value='oldest'>Oldest First</option>
+<option value='expensive'>Most Expensive</option>
+<option value='inexpensive'>Least Expensive</option>
+</select>
 </form></center>";
 foreach ($item_data as $item) {
     echo"<div style='display:inline-block;'>";
     echo"<div class='card' style='padding:10px;margin-left:20px;background-color:#343538;overflow:hidden;max-width:225px;height:370px;'>";
-	echo"<img style='width:200px;' src='$item->thumbnail'></img>";
+	echo"<a href='https://brick-hill.com/shop/$item->id'><img style='width:200px;' src='$item->thumbnail'></img></a>";
 	echo "<br />";
 	echo"<p class='ellipsis'>$item->name</p>";
 	echo"<br />";
@@ -43,7 +54,12 @@ foreach ($item_data as $item) {
 	}else{
 	    
 	}
+	
+	if($type == "head"){
+	    echo"";
+	}else{
 	echo"<a style='height:40px;font-size:12px;text-align:center;width:100%;' class='button green mobile-fill' href='https://api.brick-hill.com/v1/games/retrieveAsset?id=$item->id&amp;type=png' style='font-size: 15px; padding: 10px;'>Download Texture</a>";
+	}
 	}else{
 	    echo"<text style='color:red;'>Item ID is above 255563</text>";
 	}
@@ -59,10 +75,7 @@ die();
 <h2>Works with items below the id of 255563, anything higher will not work.</h2>
 <h3>Lookup Item</h3>
 <form method='post'>
-<label>Item Name</label>
-<input type='text' name='iid' placeholder='Item Name'>
-<br />
-<label>Category</label>
+<input style='width:170px;height:35px;' type='text' name='iid' autocorrect='off' placeholder='Item Name'>
 <select style='width:170px;height:35px;' name="type" class="select">
 <option value="all">All</option>
 <option value="hat">Hats</option>
@@ -72,6 +85,13 @@ die();
 <option value="tshirt">T-Shirts</option>
 <option value="pants">Pants</option>
 <option value="head">Heads</option>
+</select>
+<select style='width:170px;height:35px;' name="sort" class="select">
+<option value="updated">Recently Updated</option>
+<option value="newest">Newest First</option>
+<option value="oldest">Oldest First</option>
+<option value="expensive">Most Expensive</option>
+<option value="inexpensive">Least Expensive</option>
 </select>
 </form>
 <br><br>
